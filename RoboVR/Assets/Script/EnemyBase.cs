af9b723c2036b1c0a80f3859fdb71ef8d5ca2b2b
kakeru4 <kakeru.0413-sao@i.softbank.jp>
2017-12-13 02:50:08 +0900

    ファイル追加

---
+++ RoboVR/Assets/Script/EnemyBase.cs
@@ -0,0 +1,14 @@
+﻿//--------------------------------------------------------------------------------------------------
+// @file	EnemyBase.cs
+// @brief	EnemyBaseの実装
+// @author	matsuda
+//--------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+
+}
---
+++ RoboVR/Assets/Script/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿//--------------------------------------------------------------------------------------------------
+// @file	EnemyManager.cs
+// @brief	EnemyManagerの実装
+// @author	matsuda
+//--------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}

0055f1f863282a55571281573d7c66eb24db4fb5
kakeru4 <kakeru.0413-sao@i.softbank.jp>
2017-12-19 17:40:53 +0900

    敵の出現処理完了

--- RoboVR/Assets/Script/EnemyManager.cs
+++ RoboVR/Assets/Script/EnemyManager.cs
@@ -7,19 +7,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
+    public GameObject NomalEnemy;
 
-	// Use this for initialization
-	void Start ()
+    public GameObject[] Enemys;
+
+    public GameObject Target;
+
+    NavMeshAgent agent;
+
+    public int maxEnemy =5;
+
+    void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        agent = GetComponent<NavMeshAgent>();
+
+        Appear();
+       
+    }
+
+    void Appear()
     {
-		
-	}
+        // 地点がなにも設定されていないときに返します
+        if (Enemys.Length == 0)
+        {
+            return;
+        }
+
+        //agent.autoBraking = false;
+        //agent.destination = Target.transform.position;
+
+        Enemys = new GameObject[maxEnemy];
+
+        //周期的に実行したい場合はコルーチン
+        StartCoroutine(Exec());
+
+    }
+
+
+    IEnumerator Exec()
+    {
+        while (true)
+        {
+            Generate();
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    void Generate()
+    {
+        for (int enemyCount = 0; enemyCount < Enemys.Length; ++enemyCount)
+        {
+            if (Enemys[enemyCount] == null)
+            {
+                //敵を作成する
+                Enemys[enemyCount] = Instantiate(NomalEnemy, transform.position, transform.rotation) as GameObject;
+                return;
+            }
+        }
+    }
 }
